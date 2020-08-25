e678c614e3df9d9ec74f814235c389ad7208776e
binding-proc-8938 <naomirhames2020@gmail.com>
2020-08-25 16:32:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+require "pry"
+ 
+def line(deli)
+  if deli.empty?
+    puts "The line is currently empty."
+  else 
+  line_string = "The line is currently:"
+   deli.each.with_index(1) do | person, i|
+     line_string << " #{i}. #{person}"
+    end
+  puts line_string
+  end
+end 
+def take_a_number(deli, name)
+  deli << name
+  puts "Welcome, #{name}. You are number #{deli.length} in line"
+ end
+ 
