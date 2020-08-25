807762b1265684d52ef72d585bf2785b5b169b60
binding-proc-8938 <naomirhames2020@gmail.com>
2020-08-25 17:00:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,6 +16,6 @@ def take_a_number(deli, name)
   puts "Welcome, #{name}. You are number #{deli.length} in line."
  end
  def now_serving(deli)
-  puts ""
+  puts "There is nobody in line to be served."
 end
    
